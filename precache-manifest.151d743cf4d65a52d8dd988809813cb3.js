f3e5f6607493185c7a55c490859898aaf4b86029
Louay <louay.guetat@esprit.tn>
2023-08-15 19:05:25 +0100

    Updates

---
+++ precache-manifest.151d743cf4d65a52d8dd988809813cb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d1aa5d371295803ae7bc4dc244248ed",
+    "url": "/Louay-Guetat/index.html"
+  },
+  {
+    "revision": "c31442fc21d49b5bdcc0",
+    "url": "/Louay-Guetat/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "509f2e2f207397ed6f20",
+    "url": "/Louay-Guetat/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "c31442fc21d49b5bdcc0",
+    "url": "/Louay-Guetat/static/js/2.1f3d622f.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Louay-Guetat/static/js/2.1f3d622f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "509f2e2f207397ed6f20",
+    "url": "/Louay-Guetat/static/js/main.fd686574.chunk.js"
+  },
+  {
+    "revision": "a2dbf185f4bc9a8b05c4",
+    "url": "/Louay-Guetat/static/js/runtime-main.61a92791.js"
+  }
+]);
